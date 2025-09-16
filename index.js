12a5ad322824000f2568f019dbffe87619de0355
edwardomondi1 <edward.omondi1@student.moringaschool.com>
2025-09-16 12:21:19 +0300

    message

--- index.js
+++ index.js
@@ -6,4 +6,21 @@ let mostProfitableNeighborhood = 'Chelsea';
 
 let companyCeo = 'Susan Smith';
 
+// Define saturdayFun using function declaration
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Define mondayWork using function expression
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// Define wrapAdjective
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
 
